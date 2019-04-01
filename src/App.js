7b9c938d732bd303153d9802c3322926ce8ae3f2
Duc Vu <de.yd315@gmail.com>
2019-03-31 21:02:02 -0500

    added night mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Portfolio from './scene/Shared/Portfolio'
-
+import Menu from './scene/Shared/Menu'
 class App extends Component {
+
+  constructor() {
+    super();
+  }
   render() {
     return (
       <div className="App">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <div className="menu"></div>
+        <Menu/>
       </div>
     );
   }
---
+++ src/scene/Shared/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/menu.css'
+const Menu = props => {
+
+    return(
+        <ul className="menu">
+            <li className="toggle-nightmode"
+            onClick={toggleNightMode}>Night Mode</li>
+        </ul>
+    )
+}
+
+export default Menu;
+
+const toggleNightMode = () => {
+    document.querySelector(":root").classList.toggle('night-mode');
+}
